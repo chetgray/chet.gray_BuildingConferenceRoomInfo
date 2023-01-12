778c33037eecb2e8135f4fbfb0e4027712be8860
Chet Gray <chet.gray@waystar.com>
2023-01-12 16:02:12 -0500

    ✨ feat: annotate ViewModel properties

--- BuildingConferenceRoomInfo.Web/ViewModels/ConferenceRoomViewModel.cs
+++ BuildingConferenceRoomInfo.Web/ViewModels/ConferenceRoomViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace BuildingConferenceRoomInfo.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildingConferenceRoomInfo.Web.ViewModels
 {
     public class ConferenceRoomViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public BuildingViewModel Building { get; set; }
+
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
+
+        [Display(Name = "A/V Capable")]
         public bool IsAVCapable { get; set; }
         public int Capacity { get; set; }
     }
