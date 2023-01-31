4849c67e4c996b87f67747751f5168da32c1f52a
Chet Gray <chet.gray@waystar.com>
2023-01-31 14:34:28 -0500

    ✨ feat(Data): implement DAL

---
+++ BuildingConferenceRoomInfo.Data/DataAccess/DAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BuildingConferenceRoomInfo.Data.DataAccess
+{
+    internal class DAL : IDAL
+    {
+        private readonly string _connectionString;
+
+        internal DAL(string connectionString)
+        {
+            _connectionString =
+                connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public DataTable ExecuteStoredProcedure(
+            string storedProcedureName,
+            Dictionary<string, object> parameters
+        )
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (
+                SqlCommand command = new SqlCommand(storedProcedureName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                }
+            )
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                connection.Open();
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+                // Get the last DataTable in the result DataSet
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[dataSet.Tables.Count - 1];
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ BuildingConferenceRoomInfo.Data/DataAccess/IDAL.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace BuildingConferenceRoomInfo.Data.DataAccess
+{
+    internal interface IDAL
+    {
+        /// <summary>
+        /// Executes a stored procedure and returns the final result as a DataTable.
+        /// </summary>
+        /// <returns>The last DataTable in the result DataSet.</returns>
+        /// <param name="storedProcedureName">The name of the stored procedure to execute.</param>
+        /// <param name="parameters">The Dictionary of parameters to pass to the stored procedure.</param>
+        /// <remarks>
+        /// The stored procedure could <c>SELECT</c> multiple result tables, or nothing. The
+        /// desired result table should be the last <c>SELECT</c> in the stored procedure.
+        /// </remarks>
+        DataTable ExecuteStoredProcedure(
+            string storedProcedureName,
+            Dictionary<string, object> parameters
+        );
+    }
+}
