efffa44ef1c4f2f510de0049bd03a20453134602
Chet Gray <chet.gray@waystar.com>
2023-01-31 16:24:27 -0500

    ♻ refactor(Database): require `NOT NULL` for everything

--- BuildingConferenceRoomInfo.Database/Tables/Building.sql
+++ BuildingConferenceRoomInfo.Database/Tables/Building.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[Building] (
       [Id]                  INT            NOT NULL
         CONSTRAINT [PK_Building_Id] PRIMARY KEY
-    , [Name]                NVARCHAR(4000) NULL
-    , [AddressStreet]       NVARCHAR(4000) NULL
-    , [AddressCity]         NVARCHAR(4000) NULL
-    , [AddressState]        NVARCHAR(4000) NULL
-    , [AddressZip]          VARCHAR(8000)  NULL
-    , [AddressCountry]      NVARCHAR(4000) NULL
-    , [MainPhone]           VARCHAR(8000)  NULL
-    , [FloorCount]          INT            NULL
-    , [ConferenceRoomCount] INT            NULL
+    , [Name]                NVARCHAR(4000) NOT NULL
+    , [AddressStreet]       NVARCHAR(4000) NOT NULL
+    , [AddressCity]         NVARCHAR(4000) NOT NULL
+    , [AddressState]        NVARCHAR(4000) NOT NULL
+    , [AddressZip]          VARCHAR(8000)  NOT NULL
+    , [AddressCountry]      NVARCHAR(4000) NOT NULL
+    , [MainPhone]           VARCHAR(8000)  NOT NULL
+    , [FloorCount]          INT            NOT NULL
+    , [ConferenceRoomCount] INT            NOT NULL
 )
 ;
--- BuildingConferenceRoomInfo.Database/Tables/ConferenceRoom.sql
+++ BuildingConferenceRoomInfo.Database/Tables/ConferenceRoom.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[ConferenceRoom] (
       [Id]           INT            NOT NULL
         CONSTRAINT [PK_ConferenceRoom_Id] PRIMARY KEY
-    , [Name]         NVARCHAR(4000) NULL
-    , [BuildingName] NVARCHAR(4000) NULL
-    , [Phone]        VARCHAR(8000)  NULL
-    , [IsAVCapable]  BIT            NULL
-    , [Capacity]     INT            NULL
+    , [Name]         NVARCHAR(4000) NOT NULL
+    , [BuildingName] NVARCHAR(4000) NOT NULL
+    , [Phone]        VARCHAR(8000)  NOT NULL
+    , [IsAVCapable]  BIT            NOT NULL
+    , [Capacity]     INT            NOT NULL
 )
 ;
